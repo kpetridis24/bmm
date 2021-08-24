b91671b92aae1ea4f364cf4a5ae6f6cc7f8050c8
georgegito <gitopoulos99@gmail.com>
2021-08-24 17:08:37 +0300

    Fix reader

--- matlab/reader.m
+++ matlab/reader.m
@@ -1,24 +1,25 @@
+clear;
+
 %% com-Youtube
 % fid = fopen('com-Youtube.mtx');
 % e = textscan(fid,'%f %f', 2987624);
 % data = cell2mat(e);
 % fclose(fid);
 % 
-% S = sparse(data(:, 1), data(:, 2), 1)
+% S = sparse(data(:, 1), data(:, 2), 1, 1134890, 1134890, 2987624);
 
 %% s6
-% fid = fopen('s6.mtx');
-% e = textscan(fid,'%f %f', 12);
-% data = cell2mat(e);
-% fclose(fid);
-% 
-% S = sparse(data(:, 1), data(:, 2), 1)
-
-%% s12
-
-fid = fopen('s12.mtx');
-e = textscan(fid,'%f %f', 19);
+fid = fopen('s6.mtx');
+e = textscan(fid,'%f %f', 12);
 data = cell2mat(e);
 fclose(fid);
 
-S = sparse(data(:, 1), data(:, 2), 1)
+S = sparse(data(:, 1), data(:, 2), 1, 6, 6, 12)
+
+%% s12
+% fid = fopen('s12.mtx');
+% e = textscan(fid,'%f %f', 19);
+% data = cell2mat(e);
+% fclose(fid);
+% 
+% S = sparse(data(:, 1), data(:, 2), 1, 12, 12, 19);
