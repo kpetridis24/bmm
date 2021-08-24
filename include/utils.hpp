8a1afff5a2252350263b7bf93e7e535b5ab9a056
kpetridis24 <petridkon@gmail.com>
2021-08-24 13:47:14 +0300

    project template setup

--- include/utils.hpp
+++ include/utils.hpp
@@ -3,13 +3,107 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
+#include <chrono>
 
 namespace util{
+
+    void readmtxvalues(std::string f, int &n, int &nnz) 
+{
+    // Open the file:
+    std::ifstream fin(f);
+    // Declare variables:
+    int M, N;
+    // Ignore headers and comments:
+    while (fin.peek() == '%') fin.ignore(2048, '\n');
+    // Read defining parameters:
+    fin >> M >> N >> nnz;
+    if (M == N) {
+        n = M;
+    }
+    else {
+        std::cout << "not square" << std::endl;
+    }
+    fin.close();
+}
+
+// read coo in 0-based format
+void openmtxfile(std::string f, int *row, int *col, int &n, int &nnz)
+{
+    // Open the file:
+    std::ifstream fin(f);
+    // Declare variables:
+    int M, N;
+    // Ignore headers and comments:
+    while (fin.peek() == '%') fin.ignore(2048, '\n');
+    // Read defining parameters:
+    fin >> M >> N >> nnz;
+
+    for (int i = 0; i < nnz; i++){
+        fin >> row[i] >> col[i];
+        row[i]--;
+        col[i]--;
+        // i++;
+        // row[i]=col[i-1];
+        // col[i]=row[i-1];
+    }
+    fin.close();
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                 COO to CSR                                 */
+/* -------------------------------------------------------------------------- */
+
+int coo2csr(
+  int       * const row,       
+  int       * const col,     
+  int const * const row_coo,
+  int const * const col_coo,  
+  int const         nnz,      
+  int const         n,         
+  int const         isOneBased 
+) {
+
+  // ----- cannot assume that input is already 0!
+    for(int l = 0; l < n+1; l++) row[l] = 0;
+
+
+  // ----- find the correct column sizes
+    for(int l = 0; l < nnz; l++)
+        row[row_coo[l] - isOneBased]++;
+
+  // ----- cumulative sum
+    for(int i = 0, cumsum = 0; i < n; i++) {
+        int temp = row[i];
+        row[i] = cumsum;
+        cumsum += temp;
+    }
+    row[n] = nnz;
+  // ----- copy the row indices to the correct place
+    for(int l = 0; l < nnz; l++) {
+        int row_l = row_coo[l] - isOneBased;
+
+        int dst = row[row_l];
+        col[dst] = col_coo[l] - isOneBased;
+
+        row[row_l]++;
+    }
+  // ----- revert the column pointers
+    for(int i = 0, last = 0; i < n; i++) {
+        int temp = row[i];
+        row[i] = last;
+        last = temp;
+    }
+
+    return n;
+}
+
 
     void printArray(int *arr, int len){
 
         for(int i = 0; i < len; i++)
-            std::cout << arr[i] << std::endl;
+            std::cout << arr[i] << " ";
+        std::cout<<std::endl;
     }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,39 @@
 
 int main(){
 
+    int n;
+    int nnz;
+    int b;
+
+    std::string graph = "s12.mtx";
+    std::string file = "graphs/" + graph;
+
+/* -------------------------------------------------------------------------- */
+/*                                  read file                                 */
+/* -------------------------------------------------------------------------- */
+
+    util::readmtxvalues(file, n, nnz);
+
+    int *coo_row = new int[nnz];
+    int *coo_col = new int[nnz];
+
+    util::openmtxfile(file, coo_col, coo_row, n, nnz);
+    
+/* -------------------------------------------------------------------------- */
+/*                             convert COO to CSR                             */
+/* -------------------------------------------------------------------------- */
+    std::cout<<n<<", "<<nnz<<std::endl;
+    int *rowPtr = new int[n];
+    int *colInd = new int[nnz];
+
+    util::coo2csr(rowPtr, colInd, coo_row, coo_col, nnz, n, 0);
+
+    std::cout << "\nCSR row_ptr:\t";
+    util::printArray(rowPtr, n);
+    std::cout << "CSR col_ind:\t";
+    util::printArray(colInd, nnz);
+
+    delete[] coo_row;
+    delete[] coo_col;
     
 }
